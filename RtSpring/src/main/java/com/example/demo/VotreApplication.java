dfecce129b466ecd79afd7a4c4605408010168fe
Yassine-Fellous <91788542+Yassine-Fellous@users.noreply.github.com>
2023-09-19 17:17:49 +0200

    Add files via upload

---
+++ RtSpring/src/main/java/com/example/demo/VotreApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotreApplication.class, args);
+    }
+}
